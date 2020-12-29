e7407d31ab80e8dbe77c03ee1ce0f34fab752c6f
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2020-12-29 13:27:01 +0300

    Update ring buffer test

--- src/test/scala/dev/fshp/spsc/RingBufferSuite.scala
+++ src/test/scala/dev/fshp/spsc/RingBufferSuite.scala
@@ -1,21 +1,28 @@
 package dev.fshp.spsc
 
 import munit._
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
 
-class RingBufferSuite extends FunSuite {
-  test("validate read / write") {
-    val buffer = new RingBuffer(1024)
+class RingBufferSuite extends ScalaCheckSuite {
 
-    for (i <- 0 until buffer.size - 1) {
-      assertEquals(buffer.isReadable, i > 0)
-      assertEquals(buffer.isWritable, i < buffer.size - 1)
-      buffer.write(i)
-    }
+  private val sizeGen: Gen[Int] = Gen.choose(2, 1024)
+  
+  property("validate read / write") {
+    forAll (sizeGen) { size =>
+      val buffer = new RingBuffer(size)
 
-    for (i <- 0 until buffer.size - 1) {
-      assertEquals(buffer.isWritable, i > 0)
-      assertEquals(buffer.isReadable, i < buffer.size - 1)
-      buffer.read()
+      for (i <- 0 until buffer.size - 1) {
+        assertEquals(buffer.isReadable, i > 0)
+        assertEquals(buffer.isWritable, i < buffer.size - 1)
+        buffer.write(i)
+      }
+
+      for (i <- 0 until buffer.size - 1) {
+        assertEquals(buffer.isWritable, i > 0)
+        assertEquals(buffer.isReadable, i < buffer.size - 1)
+        buffer.read()
+      }
     }
   }
 }
