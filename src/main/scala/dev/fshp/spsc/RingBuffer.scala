30587d866e25564a05cd48c06994870e0224d549
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2020-12-29 13:31:19 +0300

    Refactor

--- src/main/scala/dev/fshp/spsc/RingBuffer.scala
+++ src/main/scala/dev/fshp/spsc/RingBuffer.scala
@@ -16,25 +16,25 @@ final class RingBuffer(val size: Int) extends Readable with Writable {
   assert(size > 1, "Size should be greater than 1")
 
   private val buffer: Array[Int] = new Array[Int](size)
-  @volatile private var readIndex: Int = 0 // next write index
-  @volatile private var writeIndex: Int = 0 // next read index
+  @volatile private var head: Int = 0 // next write index
+  @volatile private var tail: Int = 0 // next read index
 
   def isReadable: Boolean = {
-    readIndex != writeIndex
+    head != tail
   }
 
   def isWritable: Boolean = {
-    (writeIndex + 1) % size != readIndex
+    (tail + 1) % size != head
   }
 
   def read(): Int = {
-    val value = buffer(readIndex)
-    readIndex = (readIndex + 1) % size
+    val value = buffer(head)
+    head = (head + 1) % size
     value
   }
 
   def write(value: Int): Unit = {
-    buffer(writeIndex) = value
-    writeIndex = (writeIndex + 1) % size
+    buffer(tail) = value
+    tail = (tail + 1) % size
   }
 }
